413641cd7482b8c3201c083c64b73df26d88edd8
Lewis Redfern <lewis96dr@hotmail.com>
2017-07-25 16:51:35 +0000

    Deleted old Library

--- Person.java
+++
@@ -1,9 +0,0 @@
-package Library;
-
-public interface Person {
-
-	public void setName (String name);
-	public String getName();
-	public void setPersonType (String personType);
-	public String getPersonType ();
-}
